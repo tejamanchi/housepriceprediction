4a31c4b4cc0a8448f05fd97dc716c7f7054e7b38
tejamanchi <tejamanchi11@gmail.com>
2026-05-20 15:56:24 +0530

    Fixed warnings and updated project

--- house_price_rdiction.py
+++ house_price_rdiction.py
@@ -1,5 +1,5 @@
 print("Hello Machine Leanning")
-imort numpy as np 
+import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt
 data=pd.read_csv("data/Housing.csv")
@@ -7,7 +7,7 @@ print(data.head())
 x=data[['area','bedrooms','bathrooms']]
 y=data['price']
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+X_train, X_test, y_train, y_test=train_test_split(x,y,test_size=0.2,random_state=42)
 from sklearn.linear_model import LinearRegression
 model=LinearRegression()
 model.fit(X_train, y_train)
@@ -17,7 +17,7 @@ mae=mean_absolute_error(y_test,y_pred)
 r2=r2_score(y_test,y_pred)
 print("MAE:",mae)
 print("R2 Score:",r2)
-sample_house=[[5000,3,2]]
+sample_house=pd.DataFrame({'area':[5000],'bedrooms':[3],'bathrooms':[2]})
 prediction=model.predict(sample_house)
 print("Predicted price:",prediction[0])
 plt.scatter(y_test,y_pred)
